a2c18cd11c910fdb15294096fe5fd7c198fab569
Yutaro-Sanada <40193574+Yutaro-Sanada@users.noreply.github.com>
2018-07-22 21:03:36 +0900

    Update quick_sort.c

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -21,9 +21,8 @@ void quick_sort(int A[], int n){
         j++;
       }
     }
-    int x = A[j-1];
-    A[j-1] = A[0];
-    A[0] = x;
+    A[0] = A[j-1];
+    A[j-1] = pivot;
     quick_sort(A, j-1);
     quick_sort(A+j, n-j);  
     }
